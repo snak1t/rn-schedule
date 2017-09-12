15c99e0a92519d0279cb814fb6c1656e2b906917
Dmitry Rykov <dmitry_rykov@epam.com>
2017-09-12 11:33:20 +0300

    Set Data server domain to Heroku build

--- src/data-request.js
+++ src/data-request.js
@@ -1,6 +1,7 @@
 import { AsyncStorage } from 'react-native';
 
 const storageAppPrefix = '@rnSchedule';
+const dataServerDomain = 'https://node-schedule.herokuapp.com';
 
 export const getSchedule = async (university, groupId) => {
     let data = await AsyncStorage
@@ -8,7 +9,7 @@ export const getSchedule = async (university, groupId) => {
         .catch(() => null);
 
     if (!data) {
-        data = await fetch(`http://10.19.9.203:3000/${university}/${groupId}`)
+        data = await fetch(`${dataServerDomain}/${university}/${groupId}`)
             .then(response => response.json())
             .then(data => JSON.stringify(data));
         AsyncStorage.setItem('@rnSchedule:schedule', data);
